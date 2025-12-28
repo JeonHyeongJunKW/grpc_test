69f73c15df44f806036217c63cfda4114e4904ef
JeonHyeongJunKW <jhj@robotis.com>
2025-12-28 21:41:30 +0900

    Added release docker file for request server
    Signed-off-by: JeonHyeongJunKW <jhj@robotis.com>

--- counting_request_client/build.sh
+++ counting_request_client/build.sh
@@ -1,5 +1,4 @@
 function build_counting_request_client() {
-  cd /workspaces/grpc_test/counting_request_client
   rm -rf ~/counting_request_client
   cp -r ../counting_request_client ~/counting_request_client
   cp -r ../proto ~/counting_request_client/proto
